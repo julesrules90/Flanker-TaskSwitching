c236f7d5a7b92b50af8fbb9d94b39e268a1e9da6
Jules <jlvdbospoort@outlook.com>
2023-08-22 23:37:47 -0400

    replacing code

---
+++ pythoncode/main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+from config import directory
+from datacleaning import main, save_to_excel
+from analysis import *
+
+if __name__ == '__main__':
+    dataframes, combined_df = main()
+
+    skip_columns = ['Participant ID', 'Age', 'Sex', 'Flanker Trial', 'reward_Flanker', 'Task Switching Trial', 'reward_Task_Switching', 'Proportion Congruent', 'Switch Rate']
+
+    for i, df in enumerate(dataframes):
+        unique_id = int(df.loc[0, 'Participant ID'])
+        with pd.ExcelWriter(os.path.join(directory, f'combined_{unique_id}.xlsx'), engine='xlsxwriter') as writer:
+            save_to_excel(writer, df, 'Data')
+
+            stats = descriptive_statistics(df, skip_columns)
+            stats_df = pd.DataFrame(stats)
+            stats_df.index.name = 'Statistic'
+            stats_df.reset_index(inplace=True)
+
+            save_to_excel(writer, stats_df, 'Statistics')
+
+        histogram(df, unique_id, skip_columns)
+        print(f'Finished statistics and histograms for participant {unique_id}')
+
+    plot_average_histograms(combined_df)
+
+    combined_statistics_and_histogram(combined_df)
+    print('Finished creating Combined_Sample.xlsx')
+
+    plot_avg_rt_by_accuracy(combined_df)
+
+    plot_correlations_from_excel(directory)
+    print('Finished Scatterplots')
+
+    plot_avg_rt_by_difficulty_and_reward(combined_df)
+    plot_avg_accuracy_by_difficulty_and_reward(combined_df)
+
+    mult_regression(combined_df)
